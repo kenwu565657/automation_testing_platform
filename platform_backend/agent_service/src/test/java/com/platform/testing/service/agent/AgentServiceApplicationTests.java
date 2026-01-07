89acf3cdc018341aad0de26a94462eea6ce2dbdd
kenwu565657 <kenwu565656@icloud.com>
2026-01-07 23:50:05 +0800

    add agent service

---
+++ platform_backend/agent_service/src/test/java/com/platform/testing/service/agent/AgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.testing.service.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- platform_backend/settings.gradle.kts
+++ platform_backend/settings.gradle.kts
@@ -4,4 +4,5 @@ extra["springCloudVersion"] = "2025.1.0"
 
 include("common_module")
 include("gateway")
+include("agent_service")
 include("testing_engine")
