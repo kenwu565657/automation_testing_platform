b35339bccf9b478822ee626f89a080ec6c708793
kenwu565657 <kenwu565656@icloud.com>
2026-01-12 23:55:58 +0800

    update root build script

--- platform_backend/build.gradle.kts
+++ platform_backend/build.gradle.kts
@@ -2,8 +2,6 @@ extra["springCloudVersion"] = "2025.1.0"
 
 plugins {
     java
-    id("org.springframework.boot") version "4.0.1"
-    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.platform.testing"
@@ -21,18 +19,28 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.register("prepareKotlinBuildScriptModel") {
+        // no-op
+    }
+
+    if (project.name != "common_module") {
+        afterEvaluate {
+            dependencies {
+                implementation(project(":common_module"))
+            }
+        }
+    }
 }
 
 val springCloudVersion: String by project.extra
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
-    }
-}
 
 tasks.withType<Test> {
     useJUnitPlatform()
